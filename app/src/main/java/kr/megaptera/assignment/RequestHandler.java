8ca80aa65919216a34da8f8e8490f3cd82a22b19
hsw0905 <hsw0905@gmail.com>
2023-02-04 19:04:49 +0900

    feat: 라인 읽기 성공

--- app/src/main/java/kr/megaptera/assignment/RequestHandler.java
+++ app/src/main/java/kr/megaptera/assignment/RequestHandler.java
@@ -1,8 +1,16 @@
 package kr.megaptera.assignment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
-public class RequestHandler implements Runnable{
+public class RequestHandler implements Runnable {
+
+  public static final int MAX_BUFFER_SIZE = 1_000_000;
   private final Socket clientSocket;
 
   public RequestHandler(Socket clientSocket) {
@@ -12,5 +20,17 @@ public class RequestHandler implements Runnable{
   @Override
   public void run() {
     System.out.println("RequestHandler is Ready");
+    try (InputStream inputStream = clientSocket.getInputStream();
+        OutputStream outputStream = clientSocket.getOutputStream()) {
+
+      try (BufferedReader bufferedReader = new BufferedReader(
+          new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+        System.out.println(bufferedReader.readLine());
+        //Todo : 라인 단위로 읽어서 Method, Path, Body 분리
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
