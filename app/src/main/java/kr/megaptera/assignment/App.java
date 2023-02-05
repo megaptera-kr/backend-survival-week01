cd724cc9aac11573dc2be50e137df2f378bd9c37
euijin jeong <hitman1439@naver.com>
2023-02-05 19:06:51 +0900

    init : 1주차 과제 생성

--- app/src/main/java/kr/megaptera/assignment/App.java
+++ app/src/main/java/kr/megaptera/assignment/App.java
@@ -1,29 +1,60 @@
 package kr.megaptera.assignment;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.CharBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
 public class App {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         App app = new App();
         app.run();
     }
 
-    private void run(){
+    private void run() throws IOException {
         int port = 8080;
 
         Map<Long, String> tasks = new HashMap<>();
-
-        // TODO: 요구사항에 맞게 과제를 진행해주세요.
-
         // 1. Listen
-
+        ServerSocket listener = new ServerSocket(8080,0);
+        System.out.println("Listen!");
         // 2. Accept
 
-        // 3. Request
+        while (true) {
+            Socket socket = listener.accept();
+            System.out.println("Accept!");
 
-        // 4. Response
+            // 3. Request
+            Readable reader = new InputStreamReader(socket.getInputStream());
+            CharBuffer charBuffer = CharBuffer.allocate(1_000_000);
+            reader.read(charBuffer);
+
+            charBuffer.flip();
+            System.out.println(charBuffer.toString());
+            String headerData = charBuffer.toString();
+
+            String[] split = headerData.split("\n");
+
+
+            // 4. Response
+            String body = "This is body";
+            byte[] bytes = body.getBytes();
+            String message = ""
+                    + "HTTP/1.1 200 OK\n"
+                    + "Content-type: text/html; charset=UTF-8\n"
+                    + "Content-Length: " + bytes.length + "\n\n" + body;
+
+            Writer writer = new OutputStreamWriter(socket.getOutputStream());
+            writer.write(message);
+            writer.flush();
+            socket.close();
+        }
     }
 
 }
