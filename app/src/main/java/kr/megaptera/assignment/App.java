727f0fceb83562e9f4495fc97fceff8bb8b61e19
nmnnhlba <xorwnsnmnn@naver.com>
2023-02-05 21:38:39 +0900

    feat: Add Create/Read/Update/Delete Task

--- app/src/main/java/kr/megaptera/assignment/App.java
+++ app/src/main/java/kr/megaptera/assignment/App.java
@@ -29,23 +29,25 @@ public class App {
         // TODO: 요구사항에 맞게 과제를 진행해주세요.
 
         // 1. Listen
-        ServerSocket listener = new ServerSocket(port, 0);
-        System.out.println("Listen!!!");
+        ServerSocket listener = new ServerSocket(port);
 
         while (true) {
             // 2. Accept
             Socket socket = listener.accept();
-            System.out.println("Accepted!!!");
 
             // 3. Request
             String charBufferString = getCharBufferString(socket);
-            String patternString = "(GET|POST|PUT|PATCH|HEAD|OPTIONS)\\s(.+?)\\s.*\\r?\\n((.|\\n|\\r)*)Host:\\s(.+)((.|\\n|\\r)*)\\r?\\n\\r?\\n((.|\\n|\\r)*)$";
+            String patternString = "(GET|POST|PUT|PATCH|DELETE|HEAD|OPTIONS)\\s(.+?)\\s.*\\r?\\n((.|\\n|\\r)*)Host:\\s(.+)((.|\\n|\\r)*)\\r?\\n\\r?\\n((.|\\n|\\r)*)$";
             Pattern pattern = Pattern.compile(patternString);
             Matcher matcher = pattern.matcher(charBufferString);
             matcher.find();
             String requestMethod = matcher.group(1);
             String requestUrl = matcher.group(2);
             String host = matcher.group(5);
+
+            if ( requestUrl.equals("/") ) {
+                sendHelloworld(socket, host);
+            }
 
             String[] requestUrlArr = requestUrl.split("/");
             String urlPath = requestUrlArr[1];
@@ -62,21 +64,20 @@ public class App {
                 }
 
                 if ( requestMethod.equals("PATCH") ) {
-                    updateTask(socket, body, tasks, host);
+                    updateTask(socket, body, requestUrlArr, tasks, host);
                 }
 
                 if ( requestMethod.equals("DELETE") ) {
-                    deleteTask(socket, body, tasks, host);
+                    deleteTask(socket, requestUrlArr, tasks, host);
                 }
-            } else if ( requestUrl.equals("/") ) {
-
             } else {
-                System.out.println("404");
+                Writer writer = new OutputStreamWriter(socket.getOutputStream());
+                writer.write(sendResponse(0, host, 404, "Not Found"));
+                writer.flush();
             }
 
             socket.close();
         }
-
     }
 
     private static String getCharBufferString(Socket socket) throws IOException {
@@ -87,6 +88,21 @@ public class App {
         charBuffer.flip();
         String charBufferString = charBuffer.toString();
         return charBufferString;
+    }
+
+    private void sendHelloworld(Socket socket, String host) throws IOException {
+        String body = "Hello, world!";
+        byte[] bytes = body.getBytes();
+
+        String message = "" +
+                "HTTP/1.1 200 OK\n" +
+                "Content-Type: text/html; charset=UTF-8\n" +
+                "Content-Length: " + bytes.length + "\n" +
+                "\n" + body;
+
+        Writer writer = new OutputStreamWriter(socket.getOutputStream());
+        writer.write(message);
+        writer.flush();
     }
 
     private void sendTasks(Socket socket, Map<Long, String> tasks, String host) throws IOException {
@@ -134,17 +150,81 @@ public class App {
         writer.flush();
     }
 
-    private void updateTask(Socket socket, String body, Map<Long, String> tasks, String host) throws IOException {
+    private void updateTask(Socket socket, String body, String[] requestUrlArr, Map<Long, String> tasks, String host) throws IOException {
+        String message = "";
+        Writer writer = new OutputStreamWriter(socket.getOutputStream());
+        Gson gson = new Gson();
 
+        String serializedJsonTasks = "";
+        byte[] serializedJsonTaskBytes = serializedJsonTasks.getBytes();
+        JsonObject jsonTask = gson.fromJson(body, JsonObject.class);
+
+        if ( jsonTask == null || requestUrlArr.length != 3 ) {
+            message = sendResponse(serializedJsonTaskBytes.length, host, 400, "Bad Request");
+            writer.write(message);
+            writer.flush();
+            return;
+        }
+
+        Long id = Long.parseLong(requestUrlArr[2]);
+        JsonElement taskValue = jsonTask.get("task");
+
+        if ( taskValue == null ) {
+            message = sendResponse(serializedJsonTaskBytes.length, host, 400, "Bad Request");
+            writer.write(message);
+            writer.flush();
+            return;
+        }
+
+        if ( tasks.containsKey(id) ) {
+            tasks.put(id, taskValue.getAsString());
+            serializedJsonTasks = gson.toJson(tasks);
+            serializedJsonTaskBytes = serializedJsonTasks.getBytes();
+            message = sendResponse(serializedJsonTaskBytes.length, host, 200, "OK")
+                    + "\n" + serializedJsonTasks;
+        } else {
+            message = sendResponse(serializedJsonTaskBytes.length, host, 404, "Not Found");
+        }
+
+        writer.write(message);
+        writer.flush();
     }
 
-    private void deleteTask(Socket socket, String body, Map<Long, String> tasks, String host) throws IOException {
+    private void deleteTask(Socket socket, String[] requestUrlArr, Map<Long, String> tasks, String host) throws IOException {
+        String message = "";
+        Writer writer = new OutputStreamWriter(socket.getOutputStream());
+        Gson gson = new Gson();
 
+        String serializedJsonTasks = "";
+        byte[] serializedJsonTaskBytes = serializedJsonTasks.getBytes();
+
+        if ( requestUrlArr.length != 3 ) {
+            message = sendResponse(serializedJsonTaskBytes.length, host, 400, "Bad Request");
+            writer.write(message);
+            writer.flush();
+            return;
+        }
+
+        Long id = Long.parseLong(requestUrlArr[2]);
+
+        if ( tasks.containsKey(id) ) {
+            tasks.remove(id);
+            serializedJsonTasks = gson.toJson(tasks);
+            serializedJsonTaskBytes = serializedJsonTasks.getBytes();
+            message = sendResponse(serializedJsonTaskBytes.length, host, 200, "OK")
+                    + "\n" + serializedJsonTasks;
+        } else {
+            message = sendResponse(serializedJsonTaskBytes.length, host, 404, "Not Found");
+        }
+
+        writer.write(message);
+        writer.flush();
     }
 
     private String sendResponse(int contentLength, String host, int statusCode, String statusMessage) {
         return "HTTP/1.1 " + statusCode + " " + statusMessage + "\n" +
                 "Content-Type: application/json; charset=UTF-8\n" +
+                "Connection: close\n" +
                 "Host: " + host + "\n" +
                 "Content-Length: " + contentLength + "\n";
     }
