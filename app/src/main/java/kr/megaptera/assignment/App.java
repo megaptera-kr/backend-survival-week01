e1f4efbab320c38d3de411f2d9254f573c96debc
lkk6201 <lkk6201@gmail.com>
2023-02-07 00:59:46 +0900

    백엔드 1주차 과제 작성 (진행한 부분까지만 제출)

--- app/src/main/java/kr/megaptera/assignment/App.java
+++ app/src/main/java/kr/megaptera/assignment/App.java
@@ -1,29 +1,78 @@
 package kr.megaptera.assignment;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.CharBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
 public class App {
 
-    public static void main(String[] args){
+    private static Long taskId = 0L;
+
+    public static void main(String[] args) throws IOException {
         App app = new App();
         app.run();
     }
 
-    private void run(){
-        int port = 8080;
-
-        Map<Long, String> tasks = new HashMap<>();
-
-        // TODO: 요구사항에 맞게 과제를 진행해주세요.
-
-        // 1. Listen
-
-        // 2. Accept
-
-        // 3. Request
-
-        // 4. Response
+    private String getRequest(Socket socket) throws IOException {
+        Reader reader = new InputStreamReader(socket.getInputStream());
+        CharBuffer charBuffer = CharBuffer.allocate(1_000_000);
+        reader.read(charBuffer);
+        charBuffer.flip();
+        return charBuffer.toString();
     }
 
+    private String getResponse(String request, Map<Long, String> tasks) {
+        String httpMethod = request.split(" ")[0];
+        String path = request.split(" ")[1];
+        int statusCode = 0;
+        String body = "";
+
+        return createMessage(statusCode, body);
+    }
+
+    private String createMessage(int statusCode, String body) {
+        return  "" +
+                "HTTP/1.1 " + statusCode + " OK\n" +
+                "Content-Length: " + body.getBytes().length + "\n" +
+                "Content-Type: application/json; charset=UTF-8\n" +
+                "\n" +
+                body;
+    }
+
+    private void run() throws IOException {
+        Map<Long, String> tasks = new HashMap<>();
+
+        // 1. Listen
+        ServerSocket listener = new ServerSocket(8080, 0);
+        System.out.println("Listen!");
+
+        while (true) {
+            // 2. Accept
+            Socket socket = listener.accept();
+            System.out.println("Accept!");
+
+            // 3. Request
+            String request = getRequest(socket);
+
+            // 4. Response
+            String response = getResponse(request, tasks);
+
+            Writer writer = new OutputStreamWriter(socket.getOutputStream());
+            writer.write(response);
+            writer.flush();
+
+            // 5. Close
+            socket.close();
+        }
+    }
 }
