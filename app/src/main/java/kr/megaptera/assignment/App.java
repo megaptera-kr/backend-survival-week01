8e8940ee1dc9f00c4afb478643ec810d08e45f00
p-ej <show400035@naver.com>
2023-04-14 02:34:17 +0900

    feat: POST /tasks 구현

--- app/src/main/java/kr/megaptera/assignment/App.java
+++ app/src/main/java/kr/megaptera/assignment/App.java
@@ -1,6 +1,8 @@
 package kr.megaptera.assignment;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,8 +12,11 @@ import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.CharBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // 소켓을 사용한 HTTP 서버
 public class App {
@@ -29,27 +34,27 @@ public class App {
         int port = 8080;
         ServerSocket serverSocket = new ServerSocket(port, 0);
         Map<Long, String> tasks = new HashMap<>();
-        tasks.put(1L, "dd");
-
+        Long mapIndex = 0L;
 
         while (true) {
             // 2. Accept
             Socket socket = serverSocket.accept();
 
             // 3. Request
-            Reader reader = new InputStreamReader(socket.getInputStream());
+            Reader reader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
             CharBuffer charBuffer = CharBuffer.allocate(1_000_000);
             reader.read(charBuffer);
             charBuffer.flip();
 
             String[] arr = charBuffer.toString().split("\\s");
-            System.out.println("charBuffer\n" + arr[1]);
 
             switch (arr[0]) {
                 case "GET":
-                    requestGet(arr[1], socket, tasks);
+                    getRequest(arr[1], socket, tasks);
                     break;
                 case "POST":
+                    mapIndex++;
+                    postRequest(arr[1], socket, tasks, charBuffer, mapIndex);
                     break;
                 case "PATCH":
                     break;
@@ -57,29 +62,50 @@ public class App {
                     break;
             }
 
-//            responseBody(socket);
-
             socket.close();
         }
 
     }
 
-    private void requestGet(String path, Socket socket, Map<Long, String> tasks) throws IOException {
+    private void postRequest(String path, Socket socket, Map<Long, String> tasks, CharBuffer charBuffer, Long mapIndex) throws IOException {
         if (path.equals("/tasks")) {
-            responseBody(socket, tasks);
+            Pattern pattern = Pattern.compile("\\{.*\\}");
+            Matcher matcher = pattern.matcher(charBuffer.toString());
+            if (matcher.find()) {
+                String jsonBody = matcher.group();
+                Gson gson = new Gson();
+                JsonElement jsonElement = gson.fromJson(jsonBody, JsonElement.class);
+                JsonObject jsonObject = jsonElement.getAsJsonObject();
+                String task = jsonObject.get("task").getAsString();
+                tasks.put(mapIndex, task);
+                responseBody(socket, tasks, "POST");
+            }
         }
     }
 
-    private void responseBody(Socket socket, Map<Long, String> tasks) throws IOException {
+    private void getRequest(String path, Socket socket, Map<Long, String> tasks) throws IOException {
+        if (path.equals("/tasks")) {
+            responseBody(socket, tasks, "GET");
+        }
+    }
+
+    private void responseBody(Socket socket, Map<Long, String> tasks, String httpMethod) throws IOException {
         // 4. Response
         String body = new Gson().toJson(tasks);
-
+        String message = "";
         byte[] bytes = body.getBytes();
 
-        String message = """
-                HTTP/1.1 200 OK
-                Content-Type: application/json; charset=UTF-8
-                Content-Length:""" + bytes.length + "\n" + "\n" + body;
+        if (httpMethod.equals("GET")) {
+            message = """
+                    HTTP/1.1 200 OK
+                    Content-Type: application/json; charset=UTF-8
+                    Content-Length:""" + bytes.length + "\n" + "\n" + body;
+        } else if (httpMethod.equals("POST")) {
+            message = """
+                    HTTP/1.1 201 Created
+                    Content-Type: application/json; charset=UTF-8
+                    Content-Length:""" + bytes.length + "\n" + "\n" + body;
+        }
 
 
         Writer writer = new OutputStreamWriter(socket.getOutputStream());
