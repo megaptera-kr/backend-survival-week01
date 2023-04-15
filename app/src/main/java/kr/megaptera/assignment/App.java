b95c0f38bb4b34da93dca9c5a202368378744bb3
spacekay <bethjkim0311@gmail.com>
2023-04-15 22:41:17 +0900

    [spacekay-week1] Request Header / Body 추출

--- app/src/main/java/kr/megaptera/assignment/App.java
+++ app/src/main/java/kr/megaptera/assignment/App.java
@@ -2,12 +2,14 @@ package kr.megaptera.assignment;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.ServerError;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class App {
 
@@ -37,6 +39,36 @@ public class App {
                 bw.write("Socket created\n\n");
                 Socket socket = listener.accept();
 
+                // 3. Request
+
+                InputStream inputStream = socket.getInputStream();
+                byte[] bytes = new byte[1_000_000];
+                int size = inputStream.read(bytes);
+                String message = new String(bytes, 0, size);
+                bw.write(message + "------ REQUEST ------\n\n");
+                bw.flush();
+
+                StringTokenizer st = new StringTokenizer(message, "\n");
+                String[] firstLine = st.nextToken().split(" ");
+                String method = firstLine[0];
+                String[] path = firstLine[1].split("/");
+
+                boolean hasBody = false;
+
+                while (st.hasMoreElements()) {
+                    if (st.nextToken().trim().length() == 0) {
+                        hasBody = true;
+                        break;
+                    }
+                }
+
+                StringBuffer sb = new StringBuffer();
+
+                while (hasBody && st.hasMoreElements()) {
+                    sb.append(st.nextToken() + "\n");
+                }
+
+                String requestBody = sb.toString();
 
                 // Close socket
 
