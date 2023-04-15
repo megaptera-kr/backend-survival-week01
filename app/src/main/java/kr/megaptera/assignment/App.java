9907e429ae9ed0b18093fdcd53a724d4ef431ee3
p-ej <show400035@naver.com>
2023-04-15 18:01:33 +0900

    refact: 공통 함수, 상수 변수화

--- app/src/main/java/kr/megaptera/assignment/App.java
+++ app/src/main/java/kr/megaptera/assignment/App.java
@@ -24,6 +24,24 @@ enum HttpMethod {
 }
 
 public class App {
+    private final int ZERO = 0;
+    private final String BODY_JSON_REGEX = "\\{.*\\}";
+    private final String OK_STATUS_MESSAGE = """
+            HTTP/1.1 200 OK
+            Content-Type: application/json; charset=UTF-8
+            Content-Length:""";
+    private final String CREATE_STATUS_MESSAGE = """
+            HTTP/1.1 201 Created
+            Content-Type: application/json; charset=UTF-8
+            Content-Length:""";
+    private final String BAD_REQUEST_MESSAGE = """
+            HTTP/1.1 400 Bad Request
+            Content-Type: application/json; charset=UTF-8
+            Content-Length:""" + ZERO + "\n" + "\n";
+    private final String NOT_FOUND_MESSAGE = """
+            HTTP/1.1 404 Not Found
+            Content-Type: application/json; charset=UTF-8
+            Content-Length:""" + ZERO + "\n" + "\n";
 
     public static void main(String[] args) throws IOException {
         App app = new App();
@@ -52,30 +70,16 @@ public class App {
 
             String[] arr = charBuffer.toString().split("\\s");
 
-            switch (arr[0]) {
-                case "GET":
-                    getRequest(arr[1], socket, tasks);
-                    break;
-                case "POST":
-                    mapIndex = postRequest(arr[1], socket, tasks, charBuffer, mapIndex);
-                    break;
-                case "PATCH":
-                    patchRequest(arr[1], socket, tasks, charBuffer);
-                    break;
-                case "DELETE":
-                    deleteRequest(arr[1], socket, tasks);
-                    break;
-            }
+            mapIndex = routingHandler(arr, socket, tasks, mapIndex, charBuffer);
 
+            System.out.println("socket close");
             socket.close();
         }
-
     }
 
     private void deleteRequest(String path, Socket socket, Map<Long, String> tasks) throws IOException {
-        String[] pathParsing = path.split("/");
-        String pathResource = pathParsing[1];
-        if (pathResource.equals("tasks")) {
+        String[] pathParsing = pathParameterParsing(path);
+        if (pathParsing[1].equals("tasks")) {
             Long pathParameter = Long.valueOf(pathParsing[pathParsing.length - 1]);
             boolean isCheckKey = tasks.containsKey(pathParameter);
             if (!isCheckKey) {
@@ -88,46 +92,33 @@ public class App {
     }
 
     private void patchRequest(String path, Socket socket, Map<Long, String> tasks, CharBuffer charBuffer) throws IOException {
-        String[] pathParsing = path.split("/");
-        String pathResource = pathParsing[1];
-        if (pathResource.equals("tasks")) {
-            Long pathParameter = Long.valueOf(pathParsing[pathParsing.length - 1]);
-            Pattern pattern = Pattern.compile("\\{.*\\}");
-            Matcher matcher = pattern.matcher(charBuffer.toString());
+        String[] pathParsing = pathParameterParsing(path);
+        if (pathParsing[1].equals("tasks")) {
+            Matcher matcher = requestBodyJsonParser(charBuffer);
             if (matcher.find()) {
-                String jsonBody = matcher.group();
-                JsonElement jsonElement = new Gson().fromJson(jsonBody, JsonElement.class);
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                String task = jsonObject.get("task").getAsString();
+                Long pathParameter = Long.valueOf(pathParsing[pathParsing.length - 1]);
                 boolean isCheckKey = tasks.containsKey(pathParameter);
                 if (!isCheckKey) {
                     notFoundResponse(socket);
                     return;
                 }
-                tasks.put(pathParameter, task);
+                tasks.put(pathParameter, jsonDataExtract(matcher));
                 responseBody(socket, tasks, HttpMethod.PATCH);
-            } else {
-                badRequestResponse(socket);
+                return;
             }
+            badRequestResponse(socket);
         }
     }
 
     private Long postRequest(String path, Socket socket, Map<Long, String> tasks, CharBuffer charBuffer, Long mapIndex) throws IOException {
         if (path.equals("/tasks")) {
-            Pattern pattern = Pattern.compile("\\{.*\\}");
-            Matcher matcher = pattern.matcher(charBuffer.toString());
+            Matcher matcher = requestBodyJsonParser(charBuffer);
             if (matcher.find()) {
-                mapIndex++;
-                String jsonBody = matcher.group();
-                JsonElement jsonElement = new Gson().fromJson(jsonBody, JsonElement.class);
-                JsonObject jsonObject = jsonElement.getAsJsonObject();
-                String task = jsonObject.get("task").getAsString();
-                tasks.put(mapIndex, task);
+                tasks.put(++mapIndex, jsonDataExtract(matcher));
                 responseBody(socket, tasks, HttpMethod.POST);
-
-            } else {
-                badRequestResponse(socket);
+                return mapIndex;
             }
+            badRequestResponse(socket);
         }
         return mapIndex;
     }
@@ -144,41 +135,62 @@ public class App {
         String message = "";
         byte[] bytes = body.getBytes();
 
-        if (httpMethod.equals(HttpMethod.GET) || httpMethod.equals(HttpMethod.PATCH) || httpMethod.equals(HttpMethod.DELETE)) {
-            message = """
-                    HTTP/1.1 200 OK
-                    Content-Type: application/json; charset=UTF-8
-                    Content-Length:""" + bytes.length + "\n" + "\n" + body;
-        } else if (httpMethod.equals(HttpMethod.POST)) {
-            message = """
-                    HTTP/1.1 201 Created
-                    Content-Type: application/json; charset=UTF-8
-                    Content-Length:""" + bytes.length + "\n" + "\n" + body;
+        if (httpMethod == HttpMethod.GET || httpMethod == HttpMethod.PATCH || httpMethod == HttpMethod.DELETE) {
+            message = OK_STATUS_MESSAGE + bytes.length + "\n" + "\n" + body;
+        } else if (httpMethod == HttpMethod.POST) {
+            message = CREATE_STATUS_MESSAGE + bytes.length + "\n" + "\n" + body;
         }
-
-
-        Writer writer = new OutputStreamWriter(socket.getOutputStream());
-        writer.write(message);
-        writer.flush();
+        bufferWrite(message, socket);
     }
 
     private void badRequestResponse(Socket socket) throws IOException {
-        String message = """
-                HTTP/1.1 400 Bad Request
-                Content-Type: application/json; charset=UTF-8
-                Content-Length:""" + 0 + "\n" + "\n";
+        bufferWrite(BAD_REQUEST_MESSAGE, socket);
+    }
+
+    private void notFoundResponse(Socket socket) throws IOException {
+        bufferWrite(NOT_FOUND_MESSAGE, socket);
+    }
+
+    private void bufferWrite(String message, Socket socket) throws IOException {
         Writer writer = new OutputStreamWriter(socket.getOutputStream());
         writer.write(message);
         writer.flush();
     }
 
-    private void notFoundResponse(Socket socket) throws IOException {
-        String message = """
-                HTTP/1.1 404 Not Found
-                Content-Type: application/json; charset=UTF-8
-                Content-Length:""" + 0 + "\n" + "\n";
-        Writer writer = new OutputStreamWriter(socket.getOutputStream());
-        writer.write(message);
-        writer.flush();
+    private Long routingHandler(String[] arr, Socket socket, Map<Long, String> tasks, Long mapIndex, CharBuffer charBuffer) throws IOException {
+        switch (arr[0]) {
+            case "GET":
+                getRequest(arr[1], socket, tasks);
+                break;
+            case "POST":
+                mapIndex = postRequest(arr[1], socket, tasks, charBuffer, mapIndex);
+                break;
+            case "PATCH":
+                patchRequest(arr[1], socket, tasks, charBuffer);
+                break;
+            case "DELETE":
+                deleteRequest(arr[1], socket, tasks);
+                break;
+        }
+        return mapIndex;
+    }
+
+    private Matcher requestBodyJsonParser(CharBuffer charBuffer) {
+        Pattern pattern = Pattern.compile(BODY_JSON_REGEX);
+        Matcher matcher = pattern.matcher(charBuffer.toString());
+        return matcher;
+    }
+
+    private String jsonDataExtract(Matcher matcher) {
+        String jsonBody = matcher.group();
+        JsonElement jsonElement = new Gson().fromJson(jsonBody, JsonElement.class);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        String task = jsonObject.get("task").getAsString();
+        return task;
+    }
+
+    private String[] pathParameterParsing(String path) {
+        String[] pathParsing = path.split("/");
+        return pathParsing;
     }
 }
