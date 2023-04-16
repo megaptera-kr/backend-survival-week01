f52f4f7015cc921322592907d8972e494220bd33
JiWoo-1120 <didwldn97@naver.com>
2023-04-16 22:41:26 +0900

    first

--- app/src/main/java/kr/megaptera/assignment/anwswer.java
+++ app/src/main/java/kr/megaptera/assignment/anwswer.java
@@ -16,31 +16,25 @@ import java.nio.CharBuffer;
 import java.util.HashMap;
 import java.util.Map;
 
-public class App {
+public class anwswer {
     public static Long newId = 0L;
 
     public static void main(String[] args) throws IOException {
-        App app = new App();
+        anwswer app = new anwswer();
         app.run();
     }
 
     private void run() throws IOException {
         Map<Long, String> tasks = new HashMap<>();
 
-        // TODO: 요구사항에 맞게 과제를 진행해주세요.
-
         // 1. Listen
-        // 포트 열어주기
         ServerSocket listener = new ServerSocket(8080, 0);
 
-        System.out.println("Listen!");
+        System.out.println("Server listen!");
 
-
-        // 무한 루프
         while (true) {
             // 2. Accept
-            // 클라이언트 대기
-            Socket socket =  listener.accept();
+            Socket socket = listener.accept();
 
             System.out.println("Accept!");
 
@@ -53,69 +47,70 @@ public class App {
             String message = process(tasks, request);
             writeMessage(socket, message);
         }
-
-
     }
 
-    // 3. Request
-    // 서버입장에서는 요청을 받는 것이기 때문에 read를 먼저해준다. 즉, 먼저 읽어줘야한다.
     private String getRequest(Socket socket) throws IOException {
         Reader reader = new InputStreamReader(socket.getInputStream());
 
-        CharBuffer charBuffer = CharBuffer.allocate(1_000_000); // 1,000,000개의 문자를 저장할 수 있는 버퍼를 생성
+        CharBuffer charBuffer = CharBuffer.allocate(1_000_000);
         reader.read(charBuffer);
 
-        charBuffer.flip(); // flip() : 버퍼 포지션(position)과 limit을 조정하여 읽기(read) 모드에서 쓰기(write) 모드로 전환합니다.
+        charBuffer.flip();
 
         return charBuffer.toString();
     }
 
-    // HTTP 요청(request) 문자열에서 HTTP 요청 메소드를 추출하는 메소드
-    private String getRequstMethod(String request) {
+    private String getRequestMethod(String request) {
         return request.substring(0, request.indexOf("HTTP"));
     }
 
-    // getRequstMethod에서 추출한 요청 메소드를 이용해 해당 응답을 보내준다.
     private String process(Map<Long, String> tasks, String request) {
-        String method = getRequstMethod(request);
+        String method = getRequestMethod(request);
 
-        // startsWith : 문자열이 특정 문자열 또는 문자열 접두사(prefix)로 시작하는지를 검사하는 메소드
-        if (method.startsWith("GET /tasks")){
+        if (method.startsWith("GET /tasks")) {
             return processGetTask(tasks);
         }
 
-        if(method.startsWith("POST /tasks")){
+        if (method.startsWith("POST /tasks")) {
             return processPostTask(tasks, request);
         }
 
-        if (method.startsWith("PATCH /tasks/")){
+        if (method.startsWith("PATCH /tasks/")) {
             return processPatchTask(tasks, request, method);
         }
 
-        if (method.startsWith("DELETE /tasks/")){
+        if (method.startsWith("DELETE /tasks/")) {
             return processDeleteTask(tasks, method);
         }
 
         return generateMessage("", "400 Bad Request");
     }
 
-    private String processDeleteTask(Map<Long, String> tasks, String method) {
-        Long id = parseTaskId(method);
 
-        if (!tasks.containsKey(id)) {
-            return generateMessage("", "404 Not Found");
-        }
-        tasks.remove(id);
+    private String processGetTask(Map<Long, String> tasks) {
         String content = new Gson().toJson(tasks);
 
         return generateMessage(content, "200 OK");
-
     }
 
-    private String processPatchTask(Map<Long, String> tasks, String request, String method) {
+    private String processPostTask(Map<Long, String> tasks, String request) {
+        String task = parsePayload(request, "task");
+
+        if (task.equals("")) {
+            return generateMessage("", "400 Bad Request");
+        }
+
+        tasks.put(generateTaskId(), task);
+        String content = new Gson().toJson(tasks);
+
+        return generateMessage(content, "201 Created");
+    }
+
+    private String processPatchTask(
+            Map<Long, String> tasks, String request, String method) {
         Long id = parseTaskId(method);
 
-        if (!tasks.containsKey(id)){
+        if (!tasks.containsKey(id)) {
             return generateMessage("", "404 Not Found");
         }
 
@@ -131,40 +126,24 @@ public class App {
         return generateMessage(content, "200 OK");
     }
 
-    // POST /tasks/2일때 2를 return
-    private Long parseTaskId(String method) {
-        String[] parts = method.split("/");
+    private String processDeleteTask(Map<Long, String> tasks, String method) {
+        Long id = parseTaskId(method);
 
-        return Long.parseLong(parts[2].trim());
-    }
-
-    private String processPostTask(Map<Long, String> tasks, String request) {
-        String task = parsePayload(request, "task");
-
-        if(task.equals("")) {
-            return generateMessage("", "400 Bad Request");
+        if (!tasks.containsKey(id)) {
+            return generateMessage("", "404 Not Found");
         }
 
-        tasks.put(generateTaskId(), task);
+        tasks.remove(id);
         String content = new Gson().toJson(tasks);
 
-        return generateMessage(content, "201 Created");
+        return generateMessage(content, "200 OK");
     }
 
-
-    // tasks Long 부분 값 증가
-    private Long generateTaskId() {
-        newId += 1;
-        return newId;
-    }
-
-
-    // HTTP 요청(request) 문자열에서 특정 값(value) 가져오기
     private String parsePayload(String request, String value) {
         String[] lines = request.split("\n");
         String lastLine = lines[lines.length - 1];
 
-        try{
+        try {
             JsonElement jsonElement = JsonParser.parseString(lastLine);
             JsonObject jsonObject = jsonElement.getAsJsonObject();
             return jsonObject.get(value).getAsString();
@@ -173,15 +152,17 @@ public class App {
         }
     }
 
-    // Get
-    private String processGetTask(Map<Long, String> tasks) {
-        String content = new Gson().toJson(tasks);
-
-        return generateMessage(content, "200 OK");
+    private Long generateTaskId() {
+        newId += 1;
+        return newId;
     }
 
+    private Long parseTaskId(String method) {
+        String[] parts = method.split("/");
 
-    // Get 응답메시지
+        return Long.parseLong(parts[2].trim());
+    }
+
     private String generateMessage(String body, String statusCode) {
         byte[] bytes = body.getBytes();
 
@@ -194,10 +175,10 @@ public class App {
                 body;
     }
 
-    // 메세지 보내기
     private void writeMessage(Socket socket, String message) throws IOException {
         Writer writer = new OutputStreamWriter(socket.getOutputStream());
         writer.write(message);
         writer.flush();
     }
 }
+
